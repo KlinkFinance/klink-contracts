448d1b3defe8c38eccbacb0574793fb1ea21ea2f
Avinash Buddana <avinashbuddana@Avinashs-MacBook-Air.local>
2025-09-05 16:56:48 +0530

    cap by deafult take it from decimal points

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
-import {Ownable} from "@openzeppelin/contracts@4.9.6/access/Ownable.sol";
-import {SafeERC20,IERC20} from "@openzeppelin/contracts@4.9.6/token/ERC20/utils/SafeERC20.sol";
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.6/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.6/contracts/token/ERC20/utils/SafeERC20.sol";
+
+interface IERC20Decimals {
+    function decimals() external view returns (uint8);
+}
+
 contract IDOLocking is Ownable {
     using SafeERC20 for IERC20;
-    /**
-     *  @dev Structs to store user staking data.
-     */
+
     struct Deposits {
         uint256 depositAmount;
         uint256 depositTime;
@@ -15,53 +19,54 @@ contract IDOLocking is Ownable {
         uint256 rewards;
         bool paid;
     }
-    /**
-     *  @dev Structs to store interest rate change.
-     */
+
     struct Rates {
         uint64 newInterestRate;
         uint256 timeStamp;
     }
+
     mapping(address => Deposits) private deposits;
     mapping(uint64 => Rates) public rates;
     mapping(address => bool) private hasStaked;
+
     address public tokenAddress;
+    uint8 public tokenDecimals;
+
     uint256 public stakedBalance;
     uint256 public rewardBalance;
     uint256 public stakedTotal;
     uint256 public totalReward;
+
     uint64 public index;
     uint64 public rate;
-    uint256 public lockDuration;
+    uint256 public lockDuration; // in seconds
     string public name;
     uint256 public totalParticipants;
     uint256 public cap;
-    address public gnosisSafeAddress;  // Gnosis Safe address
+    address public gnosisSafeAddress;
 
     IERC20 public ERC20Interface;
-    /**
-     *  @dev Emitted when user stakes 'stakedAmount' value of tokens
-     */
+
     event Staked(
         address indexed token,
         address indexed staker_,
         uint256 stakedAmount_
     );
-    /**
-     *  @dev Emitted when user withdraws his stakings
-     */
+
     event PaidOut(
         address indexed token,
         address indexed staker_,
         uint256 amount_,
         uint256 reward_
     );
+
     /**
-     *   @param
-     *   name_ name of the contract
-     *   tokenAddress_ contract address of the token
-     *   rate_ rate multiplied by 100
-     *   lockduration_ duration in days
+     *   @param name_ name of the contract
+     *   @param tokenAddress_ contract address of the token
+     *   @param rate_ rate multiplied by 100
+     *   @param lockDuration_ duration in hours (pass plain hours)
+     *   @param cap_ cap in whole tokens (not scaled by decimals)
+     *   @param gnosisSafeAddress_ Gnosis Safe address
      */
     constructor(
         string memory name_,
@@ -74,66 +79,55 @@ contract IDOLocking is Ownable {
         require(tokenAddress_ != address(0), "Zero token address");
         require(rate_ != 0, "Zero interest rate");
         require(cap_ > 0, "Cap must be greater than zero");
-        require(gnosisSafeAddress_ != address(0), "Zero Gnosis Safe address"); // Ensure it's not a zero address
+        require(gnosisSafeAddress_ != address(0), "Zero Gnosis Safe address");
 
         name = name_;
         tokenAddress = tokenAddress_;
-        lockDuration = lockDuration_;
+        ERC20Interface = IERC20(tokenAddress_);
+        tokenDecimals = IERC20Decimals(tokenAddress_).decimals();
+
+        cap = cap_ * (10 ** uint256(tokenDecimals));
+
+        // store lock duration in SECONDS
+        lockDuration = lockDuration_ * 1 hours;
+
         rate = rate_;
         rates[index] = Rates(rate, block.timestamp);
-        cap = cap_;
-        gnosisSafeAddress = gnosisSafeAddress_; // Store Gnosis Safe address
+        gnosisSafeAddress = gnosisSafeAddress_;
     }
 
-     // Modifier to ensure that only Gnosis Safe can call
     modifier onlyGnosisSafe() {
         require(msg.sender == gnosisSafeAddress, "Not the Gnosis Safe");
         _;
     }
 
-    /**
-     *  Requirements:
-     *  `rate_` New effective interest rate multiplied by 100
-     *  @dev to set interest rates
-     *  `lockduration_' lock days
-     *  @dev to set lock duration days
-     */
-   function setRateAndLockduration(uint64 rate_, uint256 lockduration_)
+    function setRateAndLockduration(uint64 rate_, uint256 lockDurationInHours)
         external
-        onlyGnosisSafe  // Only Gnosis Safe can call this
+        onlyGnosisSafe
     {
         require(rate_ != 0, "Zero interest rate");
-        require(lockduration_ != 0, "Zero lock duration");
+        require(lockDurationInHours != 0, "Zero lock duration");
+
         rate = rate_;
         index++;
         rates[index] = Rates(rate_, block.timestamp);
-        lockDuration = lockduration_;
+
+        // store in seconds
+        lockDuration = lockDurationInHours * 1 hours;
     }
 
-    /**
-     *  Requirements:
-     *  `rewardAmount` rewards to be added to the staking contract
-     *  @dev to add rewards to the staking contract
-     *  once the allowance is given to this contract for 'rewardAmount' by the user
-     */
     function addReward(uint256 rewardAmount)
         external
         _hasAllowance(msg.sender, rewardAmount)
         returns (bool)
     {
         require(rewardAmount > 0, "Reward must be positive");
-        if (!_payMe(msg.sender, rewardAmount)) {
-            return false;
-        }
-        totalReward = totalReward + rewardAmount;
-        rewardBalance = rewardBalance + rewardAmount;
+        if (!_payMe(msg.sender, rewardAmount)) return false;
+        totalReward += rewardAmount;
+        rewardBalance += rewardAmount;
         return true;
     }
-    /**
-     *  Requirements:
-     *  `user` User wallet address
-     *  @dev returns user staking data
-     */
+
     function userDeposits(address user)
         external
         view
@@ -153,16 +147,11 @@ contract IDOLocking is Ownable {
                 deposits[user].userIndex,
                 deposits[user].paid
             );
-        }else // Return default values if user has not staked
-        return (0, 0, 0, 0, false);
+        } else {
+            return (0, 0, 0, 0, false);
+        }
     }
-    /**
-     *  Requirements:
-     *  `amount` Amount to be staked
-     /**
-     *  @dev to stake 'amount' value of tokens 
-     *  once the user has given allowance to the staking contract
-     */
+
     function stake(uint256 amount)
         external
         _realAddress(msg.sender)
@@ -170,59 +159,52 @@ contract IDOLocking is Ownable {
         returns (bool)
     {
         require(amount > 0, "Can't stake 0 amount");
-        require(
-            stakedTotal + amount <= cap,
-            "Staking pool cap reached"
-        );
-
-      return (_stake(msg.sender, amount));
-
+        require(stakedTotal + amount <= cap, "Staking pool cap reached");
+        return _stake(msg.sender, amount);
     }
+
     function _stake(address from, uint256 amount) private returns (bool) {
-           
-           if (!_payMe(from, amount)) {
-            return false;
-        }
-        
-         if (!hasStaked[from]) {
+        if (!_payMe(from, amount)) return false;
+
+        if (!hasStaked[from]) {
             hasStaked[from] = true;
 
             deposits[from] = Deposits(
-            amount,
-            block.timestamp,
-            block.timestamp + (lockDuration * 3600), //lockDuration * 24 * 3600
-            index,
-            0,
-            false
-        );
-            totalParticipants = totalParticipants+1;
+                amount,
+                block.timestamp,
+                block.timestamp + lockDuration,
+                index,
+                0,
+                false
+            );
+            totalParticipants++;
         } else {
             require(
                 block.timestamp < deposits[from].endTime,
-                "Lock expired, please withdraw and stake again"
+                "Lock expired, withdraw and stake again"
             );
-            uint256 newAmount = deposits[from].depositAmount+ amount;
-            uint256 rewards = _calculate(from, block.timestamp) + deposits[from].rewards;
-            
-        deposits[from] = Deposits(
-            newAmount,
-            block.timestamp,
-            block.timestamp + (lockDuration * 3600), //lockDuration * 24 * 3600
-            index,
-            rewards,
-            false
-        );
+
+            uint256 newAmount = deposits[from].depositAmount + amount;
+            uint256 rewards =
+                _calculate(from, block.timestamp) + deposits[from].rewards;
+
+            deposits[from] = Deposits(
+                newAmount,
+                block.timestamp,
+                block.timestamp + lockDuration,
+                index,
+                rewards,
+                false
+            );
         }
 
         emit Staked(tokenAddress, from, amount);
-        stakedBalance = stakedBalance + amount;
-        stakedTotal = stakedTotal+amount;
-        
+        stakedBalance += amount;
+        stakedTotal += amount;
+
         return true;
     }
-    /**
-     * @dev to withdraw user stakings after the lock period ends.
-     */
+
     function withdraw() external _realAddress(msg.sender) returns (bool) {
         require(hasStaked[msg.sender], "No stakes found for user");
         require(
@@ -230,22 +212,23 @@ contract IDOLocking is Ownable {
             "Requesting before lock time"
         );
         require(!deposits[msg.sender].paid, "Already paid out");
-        return (_withdraw(msg.sender));
+        return _withdraw(msg.sender);
     }
+
     function _withdraw(address from) private returns (bool) {
-         uint256 reward = _calculate(from, deposits[from].endTime);
-        reward = reward + deposits[from].rewards;
+        uint256 reward = _calculate(from, deposits[from].endTime) +
+            deposits[from].rewards;
         uint256 amount = deposits[from].depositAmount;
 
         require(reward <= rewardBalance, "Not enough rewards");
 
-        stakedBalance = stakedBalance - amount;
-        rewardBalance = rewardBalance - reward;
+        stakedBalance -= amount;
+        rewardBalance -= reward;
         deposits[from].paid = true;
         hasStaked[from] = false;
-        totalParticipants = totalParticipants - 1;
+        totalParticipants--;
 
-        if (_payDirect(from, amount + reward )) {
+        if (_payDirect(from, amount + reward)) {
             emit PaidOut(tokenAddress, from, amount, reward);
             return true;
         }
@@ -263,32 +246,33 @@ contract IDOLocking is Ownable {
             "Requesting before lock time"
         );
         require(!deposits[msg.sender].paid, "Already paid out");
-        return (_emergencyWithdraw(msg.sender));
+        return _emergencyWithdraw(msg.sender);
     }
 
     function _emergencyWithdraw(address from) private returns (bool) {
         uint256 amount = deposits[from].depositAmount;
-        stakedBalance = stakedBalance-amount;
+        stakedBalance -= amount;
         deposits[from].paid = true;
-        hasStaked[from] = false; //Check-Effects-Interactions pattern
-        totalParticipants = totalParticipants-1;
+        hasStaked[from] = false;
+        totalParticipants--;
+
         bool principalPaid = _payDirect(from, amount);
         require(principalPaid, "Error paying");
         emit PaidOut(tokenAddress, from, amount, 0);
         return true;
     }
-    /**
-     *  Requirements:
-     *  `from` User wallet address
-     * @dev to calculate the rewards based on user staked 'amount'
-     * 'userIndex' - the index of the interest rate at the time of user stake.
-     * 'depositTime' - time of staking
-     */
+
     function calculate(address from) external view returns (uint256) {
         return _calculate(from, deposits[from].endTime);
     }
-    function _calculate(address from,uint256 endTime) private view returns (uint256) {
+
+    function _calculate(address from, uint256 endTime)
+        private
+        view
+        returns (uint256)
+    {
         if (!hasStaked[from]) return 0;
+
         (uint256 amount, uint256 depositTime, uint64 userIndex) = (
             deposits[from].depositAmount,
             deposits[from].depositTime,
@@ -298,35 +282,35 @@ contract IDOLocking is Ownable {
         uint256 time;
         uint256 interest;
         uint256 _lockduration = deposits[from].endTime - depositTime;
+
         for (uint64 i = userIndex; i < index; i++) {
-            //loop runs till the latest index/interest rate change
             if (endTime < rates[i + 1].timeStamp) {
-                //if the change occurs after the endTime loop breaks
                 break;
             } else {
                 time = rates[i + 1].timeStamp - depositTime;
-                interest = amount * (rates[i].newInterestRate)*(time)/(
-                    _lockduration * 10000
-                );
-                amount = amount+ interest;
+                interest =
+                    (amount * (rates[i].newInterestRate) * time) /
+                    (_lockduration * 10000);
+                amount += interest;
                 depositTime = rates[i + 1].timeStamp;
                 userIndex++;
             }
         }
 
         if (depositTime < endTime) {
-            //final calculation for the remaining time period
             time = endTime - depositTime;
-
-            time = endTime - depositTime;
-            interest = (time * amount * rates[userIndex].newInterestRate) / (_lockduration * 10000); //replace with (lockduration * 10000)
+            interest =
+                (time * amount * rates[userIndex].newInterestRate) /
+                (_lockduration * 10000);
         }
 
-        return (interest);
+        return interest;
     }
+
     function _payMe(address payer, uint256 amount) private returns (bool) {
         return _payTo(payer, address(this), amount);
     }
+
     function _payTo(
         address allower,
         address receiver,
@@ -336,20 +320,23 @@ contract IDOLocking is Ownable {
         ERC20Interface.safeTransferFrom(allower, receiver, amount);
         return true;
     }
+
     function _payDirect(address to, uint256 amount) private returns (bool) {
         ERC20Interface = IERC20(tokenAddress);
         ERC20Interface.safeTransfer(to, amount);
         return true;
     }
+
     modifier _realAddress(address addr) {
         require(addr != address(0), "Zero address");
         _;
     }
+
     modifier _hasAllowance(address allower, uint256 amount) {
-        // Make sure the allower has provided the right allowance.
         ERC20Interface = IERC20(tokenAddress);
-        uint256 ourAllowance = ERC20Interface.allowance(allower, address(this));
-        require(amount <= ourAllowance, "Make sure to add enough allowance");
+        uint256 ourAllowance =
+            ERC20Interface.allowance(allower, address(this));
+        require(amount <= ourAllowance, "Not enough allowance");
         _;
     }
 }
