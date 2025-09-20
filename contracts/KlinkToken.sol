c671960cbb06abb8fbb4abd4acf45ff0424f12f9
Avinash Buddana <avinashbuddana@Avinashs-MacBook-Air.local>
2025-09-20 11:58:02 +0530

    token contract moint functionality added

--- contracts/KlinkToken.sol
+++ contracts/KlinkToken.sol
@@ -8,12 +8,12 @@ import "@openzeppelin/contracts@4.9.6/access/Ownable.sol";
 
 /**
  * @title KlinkToken
- * @dev Implementation of the ERC20 token with burn functionality and fixed supply.
+ * @dev Implementation of the ERC20 token with burn and mint functionality, owned by a deployer
  */
 contract KlinkToken is ERC20, ERC20Burnable, Ownable {
 
     /**
-     * @dev Constructor initializes the ERC20 token with a name, symbol, and fixed initial supply.
+     * @dev Constructor initializes the ERC20 token with a name, symbol, and initial supply.
      *      The initial supply is assigned to the deployer of the contract.
      * @param name Name of the token.
      * @param symbol Symbol of the token.
@@ -25,6 +25,16 @@ contract KlinkToken is ERC20, ERC20Burnable, Ownable {
         uint256 initialSupply
     ) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    /**
+     * @dev Mint new tokens.
+     *      Can only be called by the owner.
+     * @param to Address to receive the newly minted tokens.
+     * @param amount Amount of tokens to mint.
+     */
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
     /**
@@ -50,7 +60,8 @@ contract KlinkToken is ERC20, ERC20Burnable, Ownable {
 
     /**
      * @dev Hook that is called before any transfer of tokens. This includes
-     *      minting (only during deployment) and burning.
+     *      minting and burning.
+     *      Overrides the `_beforeTokenTransfer` hook in ERC20.
      * @param from Address sending the tokens.
      * @param to Address receiving the tokens.
      * @param amount Amount of tokens being transferred.
